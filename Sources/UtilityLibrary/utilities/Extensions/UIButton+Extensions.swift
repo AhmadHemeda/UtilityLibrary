190815ef9615c9d2a3ac99b609a6748b264c179b
AhmadHemeda <77977659+AhmadHemeda@users.noreply.github.com>
2024-03-11 14:36:23 +0200

    Added: More functionalities to the UIButton extension

--- Sources/UtilityLibrary/utilities/Extensions/UIButton+Extensions.swift
+++ Sources/UtilityLibrary/utilities/Extensions/UIButton+Extensions.swift
@@ -1,5 +1,67 @@
 import UIKit
 
+// MARK: - Property for state
+
+extension UIButton {
+    public var title: String? {
+        get { return self.title(for: .normal) }
+        set { setTitle(newValue, for: .normal) }
+    }
+    
+    public var titleFont: UIFont? {
+        get { return titleLabel?.font }
+        set { titleLabel?.font = newValue }
+    }
+    
+    public var attributedTitle: NSAttributedString? {
+        get { return self.attributedTitle(for: .normal) }
+        set { setAttributedTitle(newValue, for: .normal) }
+    }
+    
+    public var titleColor: UIColor? {
+        get { return self.titleColor(for: .normal) }
+        set {
+            setTitleColor(newValue, for: .normal)
+            setTitleColor(newValue?.withAlphaComponent(0.5), for: .disabled)
+            setTitleColor(newValue, for: .selected)
+            if buttonType == .custom {
+                setTitleColor(newValue?.withAlphaComponent(0.5), for: .highlighted)
+            }
+        }
+    }
+    
+    public var titleShadowColor: UIColor? {
+        get { return self.titleShadowColor(for: .normal) }
+        set {
+            setTitleShadowColor(newValue, for: .normal)
+            setTitleShadowColor(newValue?.withAlphaComponent(0.5), for: .disabled)
+            setTitleShadowColor(newValue, for: .selected)
+        }
+    }
+    
+    public var image: UIImage? {
+        get { return self.image(for: .normal) }
+        set {
+            setImage(newValue?.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+    }
+    
+    public var selectedImage: UIImage? {
+        get { return self.image(for: .selected) }
+        set { setImage(newValue?.withRenderingMode(.alwaysOriginal), for: .selected) }
+    }
+    
+    public var selectedBackgroundImage: UIImage? {
+        get { return self.backgroundImage(for: .selected) }
+        set { setBackgroundImage(newValue?.withRenderingMode(.alwaysOriginal), for: .selected) }
+    }
+    
+    public var disabledBackgroundImage: UIImage? {
+        get { return self.backgroundImage(for: .disabled) }
+        set { setBackgroundImage(newValue?.withRenderingMode(.alwaysOriginal), for: .disabled) }
+    }
+}
+
 extension UIButton {
     /// Set background color for a specific control state.
     ///
