c269754da4f11141e67743954277d9113cdb43e0
AhmadHemeda <77977659+AhmadHemeda@users.noreply.github.com>
2024-03-11 13:28:37 +0200

    added NibLoadingView

--- Package.swift
+++ Package.swift
@@ -5,17 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "UtilityLibrary",
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "UtilityLibrary",
             targets: ["UtilityLibrary"]),
     ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "UtilityLibrary"),
+            name: "UtilityLibrary",
+            dependencies: []),
         .testTarget(
             name: "UtilityLibraryTests",
             dependencies: ["UtilityLibrary"]),
--- Sources/UtilityLibrary/utilities/UIView/NibLoadingView.swift
+++ Sources/UtilityLibrary/utilities/UIView/NibLoadingView.swift
@@ -1,8 +1,30 @@
-//
-//  File.swift
-//  
-//
-//  Created by Ahmad Hemeda on 11/03/2024.
-//
+import UIKit
 
-import Foundation
+class NibLoadingView: UIView {
+    
+    var contentView: UIView?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        guard let view = loadViewFromNib() else { return }
+        view.frame = bounds
+        addSubview(view)
+        contentView = view
+    }
+    
+    private func loadViewFromNib() -> UIView? {
+        let nibName = String(describing: type(of: self))
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+}
